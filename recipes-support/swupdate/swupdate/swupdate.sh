c1c75cc0b0b1e137eac0b8c393926413369221e8
Nguyen Vu Hung <vuhungkt18@gmail.com>
2020-04-08 09:44:12 +0700

    swupate: generate the unique for every device and pass argument into swupdate service

--- recipes-support/swupdate/swupdate/swupdate.sh
+++ recipes-support/swupdate/swupdate/swupdate.sh
@@ -5,6 +5,15 @@
 SWUPDATE_ARGS="-v -f /etc/swupdate.cfg -p 'reboot' ${SWUPDATE_EXTRA_ARGS}"
 SWUPDATE_WEBSERVER_ARGS=""
 SWUPDATE_SURICATTA_ARGS=""
+BOARD_NAME="mt7623-bpi-r2"
+
+# Function generate MAC address random
+function mac_generate()
+{
+  new_mac_addr=`hexdump -n3 -e'/3 "00:60:2F" 3/1 ":%02X"' /dev/urandom`
+  return 0
+}
+
 
 # source all files from /etc/swupdate/conf.d and /usr/lib/swupdate/conf.d/
 # A file found in /etc replaces the same file in /usr
@@ -17,13 +26,34 @@ for f in `(test -d /usr/lib/swupdate/conf.d/ && ls -1 /usr/lib/swupdate/conf.d/;
 done
 
 		if [ "$SWUPDATE_SURICATTA_ARGS" != "" ]; then
+
+      # Determine MAC address for board
+      # first_start = 1 => the first board is started: Generate MAC address random once and store MAC address into bootloader
+      # first_start = 0 => the board don't start the first: Read MACC address from bootloader
+      first_start=`fw_printenv FIRST_START 2> /dev/null | cut -f 2 -d'='`
+
+      if [ "$first_start" == "0" ]; then
+        # Read MAC_ADDR from bootloader
+        mac_addr=`fw_printenv MAC_ADDR 2> /dev/null | cut -f 2 -d'='`
+      else
+        # setenv FIRST_START is 0
+        fw_setenv FIRST_START 0
+        # Generate MAC_ADDR random
+        mac_generate
+        mac_addr=$new_mac_addr
+        # store MAC ADDR into bootloader
+        fw_setenv MAC_ADDR $mac_addr
+      fi
+
 			# determine suricatta update status: '1' -> 'installed', report to
 			# server
 			state=`fw_printenv ustate 2> /dev/null | cut -f 2 -d'='`
-			if [ "$state" == "1" ]; then
+      if [ "$state" == "1" ]; then
       # Confirm update status to server: SUCCESS
-				SWUPDATE_SURICATTA_ARGS="-c 2"
-			fi
+			  SWUPDATE_SURICATTA_ARGS="-c 2 -i $BOARD_NAME:$mac_addr"
+      else
+        SWUPDATE_SURICATTA_ARGS="-i $BOARD_NAME:$mac_addr"
+      fi
 		fi
 
 #  handle variable escaping in a simmple way. Use exec to forward open filedescriptors from systemd open.
